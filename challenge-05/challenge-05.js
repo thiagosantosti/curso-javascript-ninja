5806be402524e83db73f9d9ea7d820cc47f59bbe
Thiago Santos <thiagosts.br@gmail.com>
2018-06-06 16:42:59 -0300

    desafio 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,24 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var qualquer = ['teste',1,true,{},'string'];
+
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function arr(x, index) {
+	return x[index];
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+arr(qualquer,1);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +27,21 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myvar(myarray,y){
+	return myarray[y];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var array2 = ['thiago',1,true,{},null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+myvar(array2,0);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +57,60 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book ( bookName ) {
+
+	     var AllBooks = {
+		'Programando em Android': {
+		qtdPaginas: 300,
+		autor:'Ricardo Lecheda',
+		editora:'Novatec'	
+		},
+		'Como Programar em Java': {
+		qtdPaginas: 600,
+		autor:'H.M Deitel',
+		editora:'Pearson'	
+		},
+		'UML do Requisito a Solução': {
+		qtdPaginas: 400,
+		autor:'Adilson Santos',
+		editora:'Saraiva'				
+		}
+
+	}
+
+	return  !bookName ? AllBooks : AllBooks[bookName];
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+
+console.log(book());
+
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
+
 */
-// ?
+
+console.log('O livro Programando em Android tem ' + book('Programando em Android').qtdPaginas + ' páginas' );
 
 /*
+
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+
+console.log('O autor do livro Programando em Android é ' + book('Programando em Android').autor );
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+
+console.log('O livro Programando em Android foi publicado pela editora ' + book('Programando em Android').editora );
